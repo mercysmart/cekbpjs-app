6d1605ff43f17994cda2198eb3d79c0e666cb3e2
christoga <christogaandre@gmail.com>
2015-11-07 09:10:08 +0700

    Added JS code that will linked users to homepage after rated the hospital

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 function rate() {
 	window.alert("Terima kasih untuk penilaian yang anda berikan, Kami akan meninjau banding dan merespon dalam waktu beberapa hari.");
-	// window.location.href='index.html'
+	window.location.href='/'
 }
