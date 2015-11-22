1c8ab10a95547d0eef51495391b769a3aefa8b5e
christoga <christogaandre@gmail.com>
2015-11-22 14:21:52 +0700

    Fix data.php broken php scripts

--- data.php
+++ data.php
@@ -1,3 +1,5 @@
-$name = "RSUP Dr. Cipto Mangunkusumo";
-$date = date("Y/m/d");
-$kamar = "Kelas 1 Ruang 2 Bed Pria";
+<?php
+  $name = "RSUP Dr. Cipto Mangunkusumo";
+  $date = date("Y/m/d");
+  $kamar = "Kelas 1 Ruang 2 Bed Pria";
+?>
